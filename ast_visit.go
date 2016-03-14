f0fd000e9e38d96923a37d9253388a21c3b6375d
Ernest Micklei <ernest.micklei@gmail.com>
2016-03-14 20:45:57 +0100

    need pointer receivers, switch on interfaces

--- ast_visit.go
+++ ast_visit.go
@@ -1,32 +1,57 @@
 package sqlparser
 
+import "log"
+
 // Visitor methods are all put in this source file to
 // reduce work when updating ast sources from vitesss upstream.
 
 type ASTVisitor interface {
-	VisitSelect(n Select)
-	VisitUnion(n Union)
-	VisitInsert(n Insert)
-	VisitUpdate(n Update)
-	VisitDelete(n Delete)
-	VisitSet(n Set)
-	VisitDDL(n DDL)
-	VisitColumnDefinition(n ColumnDefinition)
-	VisitCreateTable(n CreateTable)
-	VisitOther(n Other)
-	VisitSelectExprs(n SelectExprs)
-	VisitStarExpr(n StarExpr)
+	VisitSelect(n *Select)
+	VisitUnion(n *Union)
+	VisitInsert(n *Insert)
+	VisitUpdate(n *Update)
+	VisitDelete(n *Delete)
+	VisitSet(n *Set)
+	VisitDDL(n *DDL)
+	VisitColumnDefinition(n *ColumnDefinition)
+	VisitCreateTable(n *CreateTable)
+	VisitOther(n *Other)
+	VisitStarExpr(n *StarExpr)
+	VisitNonStarExpr(n *NonStarExpr)
 }
 
-func (n Union) Accept(v ASTVisitor)            { v.VisitUnion(n) }
-func (n Select) Accept(v ASTVisitor)           { v.VisitSelect(n) }
-func (n Insert) Accept(v ASTVisitor)           { v.VisitInsert(n) }
-func (n Update) Accept(v ASTVisitor)           { v.VisitUpdate(n) }
-func (n Delete) Accept(v ASTVisitor)           { v.VisitDelete(n) }
-func (n Set) Accept(v ASTVisitor)              { v.VisitSet(n) }
-func (n DDL) Accept(v ASTVisitor)              { v.VisitDDL(n) }
-func (n ColumnDefinition) Accept(v ASTVisitor) { v.VisitColumnDefinition(n) }
-func (n CreateTable) Accept(v ASTVisitor)      { v.VisitCreateTable(n) }
-func (n Other) Accept(v ASTVisitor)            { v.VisitOther(n) }
-func (n SelectExprs) Accept(v ASTVisitor)      { v.VisitSelectExprs(n) }
-func (n StarExpr) Accept(v ASTVisitor)         { v.VisitStarExpr(n) }
+// might not need the accept methods...
+func (n *Union) Accept(v ASTVisitor)            { v.VisitUnion(n) }
+func (n *Select) Accept(v ASTVisitor)           { v.VisitSelect(n) }
+func (n *Insert) Accept(v ASTVisitor)           { v.VisitInsert(n) }
+func (n *Update) Accept(v ASTVisitor)           { v.VisitUpdate(n) }
+func (n *Delete) Accept(v ASTVisitor)           { v.VisitDelete(n) }
+func (n *Set) Accept(v ASTVisitor)              { v.VisitSet(n) }
+func (n *DDL) Accept(v ASTVisitor)              { v.VisitDDL(n) }
+func (n *ColumnDefinition) Accept(v ASTVisitor) { v.VisitColumnDefinition(n) }
+func (n *CreateTable) Accept(v ASTVisitor)      { v.VisitCreateTable(n) }
+func (n *Other) Accept(v ASTVisitor)            { v.VisitOther(n) }
+func (n *StarExpr) Accept(v ASTVisitor)         { v.VisitStarExpr(n) }
+func (n *NonStarExpr) Accept(v ASTVisitor)      { v.VisitNonStarExpr(n) }
+
+func StatementAccept(n Statement, v ASTVisitor) {
+	switch n.(type) {
+	case *Union:
+		v.VisitUnion(n.(*Union))
+	case *Select:
+		v.VisitSelect(n.(*Select))
+	default:
+		log.Printf("StatementAccept TODO %T", n)
+	}
+}
+
+func SelectExprAccept(n SelectExpr, v ASTVisitor) {
+	switch n.(type) {
+	case *StarExpr:
+		v.VisitStarExpr(n.(*StarExpr))
+	case *NonStarExpr:
+		v.VisitNonStarExpr(n.(*NonStarExpr))
+	default:
+		log.Printf("SelectExprAccept TODO %T", n)
+	}
+}
